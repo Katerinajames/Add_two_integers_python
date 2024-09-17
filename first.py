1e439779686ef807fff39d618cef04c5dc262dac
Katerina Paparizou <76427845+Katerinajames@users.noreply.github.com>
2024-09-17 20:31:44 +0300

    Add files via upload

---
+++ first.py
@@ -0,0 +1,14 @@
+
+k=int(input("Enter an integer\n"))
+
+
+print(" Our integer is ",k)
+
+
+l=int(input("Enter a second  integer\n"))
+
+
+print(" Our second  integer is ",l )
+
+sum=k+l 
+print(" Our sum is ",sum  )
